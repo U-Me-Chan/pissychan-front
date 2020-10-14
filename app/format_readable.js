cee291fd8012831a070f62e037a31517cb949d25
Oxore <oxore@protonmail.com>
2020-10-14 11:52:07 +0300

    Linkify messages

--- app/format_readable.js
+++ app/format_readable.js
@@ -1,15 +1,20 @@
+const linkify = require('linkifyjs/html')
+
 const regex = /\n/g
 
-function formatReadable (text) {
-  if (text === undefined) {
-    return ''
-  }
-
+function renderNewlines (text) {
   return text.replace(regex, function (match) {
     if (match === '\n') {
       return '<br>'
     }
   })
+}
+
+function formatReadable (text) {
+  if (text === undefined) {
+    return ''
+  }
+  return linkify(renderNewlines(text), { className: 'linkified' })
 }
 
 module.exports = formatReadable
