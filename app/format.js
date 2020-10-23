2421969f9b8a4bd4ae7e4acb44e8fe8ca282b575
Oxore <oxore@protonmail.com>
2020-10-23 05:40:46 +0300

    Refactor message formatting

--- app/format.js
+++ app/format.js
@@ -1,13 +1,23 @@
 const linkify = require('linkifyjs/html')
 
-const regex = /\n/g
+// Must be called after htmlDefuse, so all '>' must be replaced with &gt;
+function renderMarkdown (data) {
+  const italic = /(?<!\*|\\)\*[^*\n].+?[^*|\\]\*(?!\*)/g // *italic*
+  const bold = /(?<!\*|\\\*)\*{2,2}[^*\n].+?[^*]\*{2,2}(?!\*|\\)/g // **bold**
+  const monospace = /`[^`]*`/g // `code`
+  const reply = /&gt;&gt;\d{1,}/g
+
+  return data.replace(italic, match => '<i>' + match.slice(1, -1) + '</i>')
+    .replace(bold, match => '<b>' + match.slice(2, -2) + '</b>')
+    .replace(monospace, match => '<code>' + match.slice(1, -1) + '</code>')
+    .replace(reply, match => {
+      return '<a href=\'#' + match.slice('&gt;&gt;'.length) + '\'>' +
+        match + '</a>'
+    })
+}
 
 function renderNewlines (text) {
-  return text.replace(regex, function (match) {
-    if (match === '\n') {
-      return '<br>'
-    }
-  })
+  return text.replace(/\n/g, '<br>')
 }
 
 module.exports = {
@@ -15,7 +25,9 @@ module.exports = {
     if (text === undefined) {
       return ''
     }
-    return linkify(renderNewlines(text), { className: 'linkified' })
+    return linkify(renderNewlines(renderMarkdown(text)), {
+      className: 'linkified'
+    })
   },
   formatTimestamp: (unixtime) => {
     const t = new Date(unixtime * 1000)
--- app/html_defuse.js
+++ app/html_defuse.js
@@ -1,34 +1,5 @@
-const regex = /[&<>"']/g
-const italic = /(?<!\*|\\)\*[^*\n].+?[^*|\\]\*(?!\*)/g // *italic*
-const bold = /(?<!\*|\\\*)\*{2,2}[^*\n].+?[^*]\*{2,2}(?!\*|\\)/g // **bold**
-const monospace = /`[^`]*`/g // `code`
-const reply = />>\d{1,}/g
-
-function parser (data) {
-  data = data.replace(italic, function (match) {
-    match = match.slice(1, -1)
-    return '<i>' + match + '</i>'
-  })
-
-  data = data.replace(bold, function (match) {
-    match = match.slice(2, -2)
-    return '<b>' + match + '</b>'
-  })
-
-  data = data.replace(monospace, function (match) {
-    match = match.slice(1, -1)
-    return '<code>' + match + '</code>'
-  })
-
-  data = data.replace(reply, function (match) {
-    match = match.slice(2)
-    return '<a href=\'#' + match + '\'>&gt;&gt;' + match + '</a>'
-  })
-
-  return data
-}
-
 function htmlDefuse (data) {
+  const regex = /[&<>"']/g
   return data.replace(regex, function (match) {
     if (match === '&') {
       return '&amp;'
@@ -45,4 +16,3 @@ function htmlDefuse (data) {
 }
 
 module.exports = htmlDefuse
-module.exports = parser
