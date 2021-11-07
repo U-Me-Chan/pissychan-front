2adb09f065f22f986b55061e717973a8bf6c44d3
Oxore <oxore@protonmail.com>
2021-11-07 18:00:02 +0300

    Generate ecosystem.config.js for pm2

---
+++ .github/gen_pm2_ecosystem.sh
@@ -0,0 +1,16 @@
+source ./.env
+echo "\
+module.exports = {
+  apps : [{
+    name: "pissychan",
+    script: "index.js",
+    env_production: {
+      NODE_ENV: \"development\",
+      npm_package_name: \"$npm_package_name\",
+      npm_package_version: \"$npm_package_version\",
+      sha_short: \"$sha_short\",
+      branch_name: \"$branch_name\",
+      bundle_name: \"$bundle_name\",
+    },
+  }]
+}"
