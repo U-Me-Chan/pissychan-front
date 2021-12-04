1eb754000a3628fa3f478bca9e095f195cd64f47
Oxore <oxore@protonmail.com>
2021-12-04 23:09:03 +0300

    Rehaul debug logging, stop pm2 when overwriting files

--- .github/remote_unpack.sh
+++ .github/remote_unpack.sh
@@ -2,11 +2,15 @@
 
 set -ex
 
-[ -e ${BUNDLE_NAME} ] && rm -rf ${BUNDLE_NAME}
+if [ -e ${BUNDLE_NAME} ]; do
+  pm2 del pissychan || true
+  rm -rf ${BUNDLE_NAME}
+done
 mkdir -p ${BUNDLE_NAME}
 tar xf ${BUNDLE_NAME}.tar.gz -C ${BUNDLE_NAME}
 ln -sfrvT ${BUNDLE_NAME} pissychan
 cd pissychan
+ls -la
 pm2 del pissychan || true
 pm2 start
 pm2 save
