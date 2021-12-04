51e3b5aeaef7dc5290c40c023dee6e3f052e0511
Oxore <oxore@protonmail.com>
2021-12-04 23:13:45 +0300

    Fix shell `if` syntax

--- .github/remote_unpack.sh
+++ .github/remote_unpack.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-if [ -e ${BUNDLE_NAME} ]; do
+if [ -e ${BUNDLE_NAME} ]; then
   pm2 del pissychan || true
   rm -rf ${BUNDLE_NAME}
-done
+fi
 mkdir -p ${BUNDLE_NAME}
 tar xf ${BUNDLE_NAME}.tar.gz -C ${BUNDLE_NAME}
 ln -sfrvT ${BUNDLE_NAME} pissychan
