f14238070a896b07fc044bf3568c365513b20761
Oxore <oxore@protonmail.com>
2021-12-04 21:24:18 +0300

    Save pm2 state after start, make rm less verbose

--- .github/remote_unpack.sh
+++ .github/remote_unpack.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
-[ -e ${BUNDLE_NAME} ] && rm -rfv ${BUNDLE_NAME}
+[ -e ${BUNDLE_NAME} ] && rm -rf ${BUNDLE_NAME}
 mkdir -p ${BUNDLE_NAME}
 tar xf ${BUNDLE_NAME}.tar.gz -C ${BUNDLE_NAME}
 ln -sfrvT ${BUNDLE_NAME} pissychan
 cd pissychan
 pm2 del pissychan || true
 pm2 start
+pm2 save
