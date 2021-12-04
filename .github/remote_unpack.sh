11407df3f70ba6c90e03bacf5440b59f70c768e7
Oxore <oxore@protonmail.com>
2021-12-04 21:17:23 +0300

    Fix removing existing deploy directory

--- .github/remote_unpack.sh
+++ .github/remote_unpack.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-[ -e ${BUNDLE_NAME} ] && rm rfv ${BUNDLE_NAME}
+[ -e ${BUNDLE_NAME} ] && rm -rfv ${BUNDLE_NAME}
 mkdir -p ${BUNDLE_NAME}
 tar xf ${BUNDLE_NAME}.tar.gz -C ${BUNDLE_NAME}
 ln -sfrvT ${BUNDLE_NAME} pissychan
