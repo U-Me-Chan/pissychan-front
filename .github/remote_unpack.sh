9c403a0c1bee4bfe7d4b59ec8c6fbdfddbc42925
Oxore <oxore@protonmail.com>
2021-12-04 21:00:07 +0300

    Overwrite symlink if exist

--- .github/remote_unpack.sh
+++ .github/remote_unpack.sh
@@ -5,7 +5,7 @@ set -ex
 [ -e ${BUNDLE_NAME} ] && rm rfv ${BUNDLE_NAME}
 mkdir -p ${BUNDLE_NAME}
 tar xf ${BUNDLE_NAME}.tar.gz -C ${BUNDLE_NAME}
-ln -sfrv ${BUNDLE_NAME} pissychan
+ln -sfrvT ${BUNDLE_NAME} pissychan
 cd pissychan
 pm2 del pissychan
 pm2 start
