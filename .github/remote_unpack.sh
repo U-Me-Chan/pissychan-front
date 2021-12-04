9061028077f20c5c35830a39ca2e5d00a394378e
Oxore <oxore@protonmail.com>
2021-12-04 20:56:23 +0300

    Remove directory if exist

--- .github/remote_unpack.sh
+++ .github/remote_unpack.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-mkdir ${BUNDLE_NAME}
+[ -e ${BUNDLE_NAME} ] && rm rfv ${BUNDLE_NAME}
+mkdir -p ${BUNDLE_NAME}
 tar xf ${BUNDLE_NAME}.tar.gz -C ${BUNDLE_NAME}
 ln -sfrv ${BUNDLE_NAME} pissychan
 cd pissychan
