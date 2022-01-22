72ce00789ce042f1caed1e008ae2e6265ae66087
Oxore <oxore@protonmail.com>
2022-01-22 17:59:18 +0300

    Switch environment to production

--- .github/gen_pm2_ecosystem.sh
+++ .github/gen_pm2_ecosystem.sh
@@ -7,7 +7,7 @@ module.exports = {
     name: \"pissychan\",
     script: \"index.js\",
     env_production: {
-      NODE_ENV: \"dev\",
+      NODE_ENV: \"production\",
       npm_package_name: \"$npm_package_name\",
       npm_package_version: \"$npm_package_version\",
       sha_short: \"$sha_short\",
