29d9d4a0b3b350cdc06ba48ad2973205cad3fb3a
Oxore <oxore@protonmail.com>
2022-01-23 00:32:12 +0300

    Drop chokidar in favor of nodemon

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// https://nimblewebdeveloper.com/blog/hot-reload-nodejs-server
-// Only run this in development
-if (process.env.NODE_ENV !== 'production') {
-  const chokidar = require('chokidar')
-
-  // Set up watcher to watch all files in ./app
-  const watcher = chokidar.watch('./app')
-
-  watcher.on('ready', function () {
-    // On any file change event
-    // You could customise this to only run on new/save/delete etc
-    // This will also pass the file modified into the callback
-    // however for this example we aren't using that information
-    watcher.on('all', function () {
-      console.log('Reloading server...')
-      // Loop through the cached modules
-      // The 'id' is the FULL path to the cached module
-      Object.keys(require.cache).forEach(function (id) {
-        // Get the local path to the module
-        const localId = id.substr(process.cwd().length)
-
-        // Ignore anything not in app
-        if (!localId.match(/^\/app\//)) return
-
-        // Remove the module from the cache
-        delete require.cache[id]
-      })
-      console.log('Server reloaded.')
-    })
-  })
-}
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
