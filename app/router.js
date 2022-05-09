babfb37d9703cfc1ae2f65b02247cb2d06817448
Oxore <oxore@protonmail.com>
2022-05-09 03:02:30 +0300

    We use no favicons here

--- app/router.js
+++ app/router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
+router.get('/favicon.ico', (req, res) => res.status(204).send())
 router.post('/:tag/:thread_id', require('./post'))
 router.get('/:tag/:thread_id', require('./thread'))
 router.post('/:tag/', require('./post'))
