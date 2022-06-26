7143d9b379429b2900d1a9b86c014dd3be15e155
Oxore <oxore@protonmail.com>
2022-06-26 21:46:08 +0300

    Add BACKEND_{HOSTNAME,PORT} vars to the ecosystem

--- .github/gen_pm2_ecosystem.sh
+++ .github/gen_pm2_ecosystem.sh
@@ -8,6 +8,8 @@ module.exports = {
     script: \"index.js\",
     env_production: {
       NODE_ENV: \"production\",
+      BACKEND_HOSTNAME: \"$BACKEND_HOSTNAME\",
+      BACKEND_PORT: \"$BACKEND_PORT\",
       npm_package_name: \"$npm_package_name\",
       npm_package_version: \"$npm_package_version\",
       sha_short: \"$sha_short\",
