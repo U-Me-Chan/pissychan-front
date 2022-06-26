8488c80efe76c3ea2b5534e76f6991aa0ebd8331
Oxore <oxore@protonmail.com>
2022-06-26 22:09:26 +0300

    Revert "Add BACKEND_{HOSTNAME,PORT} vars to the ecosystem"
    This reverts commit 7143d9b379429b2900d1a9b86c014dd3be15e155.

--- .github/gen_pm2_ecosystem.sh
+++ .github/gen_pm2_ecosystem.sh
@@ -8,8 +8,6 @@ module.exports = {
     script: \"index.js\",
     env_production: {
       NODE_ENV: \"production\",
-      BACKEND_HOSTNAME: \"$BACKEND_HOSTNAME\",
-      BACKEND_PORT: \"$BACKEND_PORT\",
       npm_package_name: \"$npm_package_name\",
       npm_package_version: \"$npm_package_version\",
       sha_short: \"$sha_short\",
